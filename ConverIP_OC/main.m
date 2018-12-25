e3c969baea252be36bcbba713c3d891f7228cd3d
LawrenceHan <yangfei6565@163.com>
2018-12-25 21:05:44 +0800

    added more test

--- ConverIP_OC/main.m
+++ ConverIP_OC/main.m
@@ -15,16 +15,32 @@ int main(int argc, const char * argv[]) {
 //        NSLog(@"Hello, World!");
         
         NSString *ip = @"     172.   168    . 5. 1 ";
-        NSArray *separated = [ip componentsSeparatedByString:@"."];
-        if (separated.count != 4) {
-            return 1;
-        }
         
+        
+        // if we runs over 10,000 times, C style win
         CFAbsoluteTime start = CFAbsoluteTimeGetCurrent();
-        for (int i=0; i<1000000; i++) {
+        for (int i=0; i<10000; i++) {
             ipv4ParseCStyle(ip.UTF8String);
         }
         CFAbsoluteTime end = CFAbsoluteTimeGetCurrent()-start;
+        
+        printf("ipv4ParseCStyle runs 10,000 times, costs: %f\n", end);
+        
+        start = CFAbsoluteTimeGetCurrent();
+        for (int i=0; i<10000; i++) {
+            ipv4ParseFoundation(ip.UTF8String);
+        }
+        end = CFAbsoluteTimeGetCurrent()-start;
+        
+        printf("ipv4ParseFoundation runs 10,000 times, costs: %f\n", end);
+        
+        
+        // if we runs over 1,000,000 times, Foundation win
+        start = CFAbsoluteTimeGetCurrent();
+        for (int i=0; i<1000000; i++) {
+            ipv4ParseCStyle(ip.UTF8String);
+        }
+        end = CFAbsoluteTimeGetCurrent()-start;
         
         printf("ipv4ParseCStyle runs 1,000,000 times, costs: %f\n", end);
         
